0323c7111b9f912b5296128f50ceb9b876c79613
Toni <toni.teleco@gmail.com>
2016-10-10 21:47:33 +0200

    Added Libro.java file

---
+++ src/pkgJava_Introduction/Libro.java
@@ -0,0 +1,12 @@
+package pkgJava_Introduction;
+
+public class Libro {
+	
+	String nombre;
+	boolean prestado;
+	
+	public Libro(String nombre){
+		this.nombre = nombre;
+		this.prestado = false;
+	}
+}
